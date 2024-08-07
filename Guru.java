f4bb2e916d2c1cb97edcb8136f9be4053ea879e1
aby990 <mahija_abyudaya_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:26:38 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,16 @@
+//Object Class
+public class Guru {
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+    
+    //Constructor 
+    public Guru () {
+      id = 0;
+      nama = "";
+      mapel = "";
+      alamat = "";
+    }
+
+}
